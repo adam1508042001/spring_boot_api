61d02e05eea70092db53b688c28b7827f5c808ef
AFROUNE MOHAMED ADAM <148912667+adam1508042001@users.noreply.github.com>
2024-02-02 15:58:25 +0100

    creation de la classe et les get methode de l'entité Produit_entity

--- demo/src/main/java/entities/Produits_entity.java
+++ demo/src/main/java/entities/Produits_entity.java
@@ -1,4 +1,37 @@
 package entities;
 
+//creation de la classe
 public class Produits_entity {
+    private String reference;
+    private String designation;
+    private Integer prix_semaine;
+
+
+    //creation d'un constructeur
+    public Produits_entity(String reference, String designation, Integer prix_semaine) {
+        this.reference = reference;
+        this.designation = designation;
+        this.prix_semaine = prix_semaine;
+
+    }
+
+
+    public String getreference() {
+        return reference;
+    }
+
+    public  String getDesignation() {
+
+        return designation;
+    }
+
+    public Integer getPrix_semaine() {
+        return prix_semaine;
+    }
+
+
+
+
 }
+
+
